a6eae519f669b56ba453be2eb29642a8e5c4d75c
Alberto Re <alberto.re@gmail.com>
2022-10-03 20:11:59 +0200

    Configure ESLint and relative pre-commit hook

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
 import { Pane } from 'tweakpane';
 
-function declarePane(components, cfg = {}) {
-    const pane = new Pane(cfg);
-
-    components.forEach((component) => {
-        if ('inputs' in component) {
-            const folder = pane.addFolder({
-                title: component.title,
-                expanded: component.expanded ?? true,
-            });
-            component.inputs.forEach((input) => {
-                addInput(input, folder, components);
-            });
-        } else {
-            addInput(component, pane, components);
-        }
+function addInput(input, parent, components) {
+  components[input.name] = input.default;
+  const added = parent.addInput(components, input.name, input);
+  if ('events' in input && Array.isArray(input.events)) {
+    added.on('change', (ev) => {
+      input.events.forEach((cb) => {
+        cb(ev);
+      });
     });
-
-    return pane;
+  }
 }
 
-function addInput(input, parent, components) {
-    components[input.name] = input.default;
-    const _input = parent.addInput(components, input.name, input);
-	if ('events' in input && Array.isArray(input.events)) {
-		_input.on('change', function(ev) {
-			input.events.forEach((cb) => {
-				cb(ev)
-			})
-		});
-	}
+function declarePane(components, cfg = {}) {
+  const pane = new Pane(cfg);
+
+  components.forEach((component) => {
+    if ('inputs' in component) {
+      const folder = pane.addFolder({
+        title: component.title,
+        expanded: component.expanded ?? true,
+      });
+      component.inputs.forEach((input) => {
+        addInput(input, folder, components);
+      });
+    } else {
+      addInput(component, pane, components);
+    }
+  });
+
+  return pane;
 }
 
 module.exports = declarePane;
